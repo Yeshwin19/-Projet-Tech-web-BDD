3ffa7829b32ca960d603790fbd09e97cbaeea9fa
Yeshwin19 <46407577+Yeshwin19@users.noreply.github.com>
2019-05-23 12:53:33 +0200

    Components for my Curriculum Vitae.

---
+++ includes/dbh.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "root";
+$dbName="test";
+
+if $conn =  mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName)
+
+?>
---
+++ js/script.js
@@ -0,0 +1,3 @@
+'use-strict';
+
+tinymce.init({forced_root_block : "<p></p><em></em><strong></strong>",selector:'textarea'});
